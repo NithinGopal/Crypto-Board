17c79fba33c6d040194e01513e58b86abc7d5d91
Nithin Gopal <nithingopal2015@gmail.com>
2022-07-21 01:07:05 +0530

    Completed basic Layout
    - added navbar, logo, app.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from "react";
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+
+import './App.css'
+
+import { Navbar, Exchanges, HomePage, Cryptocurrencies, CryptoDetails, News } from './components'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <div className="navbar">
-
+        <Navbar />
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            CryptoBoard <br />
+            All rights reserved.
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer">
-
-      </div>
+      
     </div>
   );
 }
