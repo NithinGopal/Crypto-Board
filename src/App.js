cdbc037ce2f0006189a3b037da65eb11e9e6079c
Nithin Gopal <nithingopal2015@gmail.com>
2022-07-20 18:23:57 +0530

    Add npm packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import React from "react";
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
     <div className="App">
-      <h2>CryptoBoard</h2>
+      <div className="navbar">
+
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
