c4515110e845263c1a8bdfb5db0c6c2f696c470c
Nithin Gopal <nithingopal2015@gmail.com>
2022-07-21 12:16:55 +0530

    Add Search filter in /cryptocurrencies

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -30,7 +30,7 @@ const HomePage = () => {
         <Title level={2} className='home-title'>Top 10 Cryptocurrencies</Title>
         <Title level={5} className='show-more'><Link to='/cryptocurrencies'>Show More...</Link></Title>
       </div>
-      <Cryptocurrencies simplified={true} />
+      <Cryptocurrencies simplified />
       <div className='home-heading-container'>
         <Title level={2} className='home-title'>Latest Crypto News</Title>
         <Title level={5} className='show-more'><Link to='/news'>Show More...</Link></Title>
